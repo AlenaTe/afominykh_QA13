37e3f3888118be14ac9846a0b9d51bc20e75c15f
AlenaTe <alenik_87@mail.ru>
2018-03-27 10:29:32 +0300

    for jenkins

--- addressbook-selenium-tests/src/test/java/com/tr/selenium/appManager/ContactHelper.java
+++ addressbook-selenium-tests/src/test/java/com/tr/selenium/appManager/ContactHelper.java
@@ -31,7 +31,7 @@ public class ContactHelper extends HelperBase{
         type(By.name("address2"),contactData.getContactAddress2());
         type(By.name("phone2"),contactData.getContactTelephoneHome2());
         type(By.name("notes"),contactData.getContactNotes());
-        new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
+//        new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
         attach(By.name("photo"),contactData.getPhoto());
     }
 
